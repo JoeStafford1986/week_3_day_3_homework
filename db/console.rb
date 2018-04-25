f259c6d2ca777a9780e0b64ebb999d0d9ed78e55
Joe Stafford <joestafford1986@gmail.com>
2018-04-25 15:23:46 +0100

    added class objects to console

--- db/console.rb
+++ db/console.rb
@@ -1,3 +1,29 @@
 require('pry')
 require_relative('../models/artist')
 require_relative('../models/album')
+
+artist1 = Artist.new({
+"name" => "Joe"
+})
+artist2 = Artist.new({
+"name" => "Davina"
+})
+artist3 = Artist.new({
+"name" => "Luis"
+})
+
+album1 = Album.new({
+"title" => "Something",
+"genre" => "country",
+"artist_id" => artist1.id
+})
+album2 = Album.new({
+"title" => "Like",
+"genre" => "pop",
+"artist_id" => artist1.id
+})
+album3 = Album.new({
+"title" => "This",
+"genre" => "blues",
+"artist_id" => artist1.id
+})
